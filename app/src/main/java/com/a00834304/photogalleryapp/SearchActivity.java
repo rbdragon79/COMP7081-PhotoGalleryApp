0f681c63ab371dd510fec163caa1629aca5ce196
Robert B <rbroyles@my.bcit.ca>
2017-09-26 01:55:42 -0700

    Created the SearchActivity

---
+++ app/src/main/java/com/a00834304/photogalleryapp/SearchActivity.java
@@ -0,0 +1,13 @@
+package com.a00834304.photogalleryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
